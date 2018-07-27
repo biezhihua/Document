17cb5e9027abe823c86daa9791a7cc2d6c4b7ec7
biezhihua <biezhihua@gmail.com>
2018-07-27 10:48:57 +0800

    Demog

--- LiveDataDemo/app/src/main/java/com/bzh/livedata/MainActivity.kt
+++ LiveDataDemo/app/src/main/java/com/bzh/livedata/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.bzh.livedata
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 
-class MainActivity : AppCompatActivity(), LifecycleOwner {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val viewModel = NameViewModel()
-        viewModel.nameLD.observe(this, Observer { })
+        // 1. 创建LiveData，并使用
+//        val viewModel = NameViewModel()
+//
+//        viewModel.nameLD.observe(this, Observer {
+//            Log.d("biezhihua", "NameViewModel $it")
+//        })
+//
+//        viewModel.nameLD.value = "biezhihua"
+
+        // 2. 自定义LiveData
+        val stackLiveData = StockLiveData()
+        stackLiveData.observe(this, Observer {
+            Log.d("biezhihua", "StockLiveData $it")
+        })
     }
 }
 
@@ -21,3 +37,16 @@ class NameViewModel : ViewModel() {
 
     val nameLD: MutableLiveData<String> get() = name
 }
+
+
+class StockLiveData() : LiveData<String>() {
+
+    protected override fun onActive() {
+        value = "StockLiveData onActive"
+    }
+
+    protected override fun onInactive() {
+        value = "StockLiveData onInactive"
+    }
+}
+
---
+++ LiveDataDemo/app/src/main/java/com/bzh/livedata/SecondActivity.kt
@@ -0,0 +1,14 @@
+package com.bzh.livedata
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
